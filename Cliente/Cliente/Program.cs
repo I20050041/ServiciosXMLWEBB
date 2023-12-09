27b5caa93f0dba0c96f1214769de1f3e1ce9775f
Jared Israel Tovar Covarrubias <71293482+I20050041@users.noreply.github.com>
2023-12-08 20:10:00 -0600

    Add files via upload

---
+++ Cliente/Cliente/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Cliente
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            await ObtenerDatosDesdeAPI();
+        }
+
+        static async Task ObtenerDatosDesdeAPI()
+        {
+            using (HttpClient cliente = new HttpClient())
+            {
+                try
+                {
+                    HttpResponseMessage respuesta = await cliente.GetAsync("https://localhost:44369/");
+
+                    if (respuesta.IsSuccessStatusCode)
+                    {
+                        string datosXML = await respuesta.Content.ReadAsStringAsync();
+                        Console.WriteLine("Datos obtenidos correctamente:");
+                        Console.WriteLine(datosXML);
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Error al obtener datos. Código de estado: {respuesta.StatusCode}");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Error: {ex.Message}");
+                }
+            }
+        }
+    }
+}
